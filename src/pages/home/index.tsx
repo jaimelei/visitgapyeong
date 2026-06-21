e54c9c7c1d4f1fd07ee9e33bbba8aee0c358fc16
Jaime Lei Indicke <jaimeindick@gmail.com>
2026-06-21 10:24:36 +0800

    001 | Init

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+    return (
+        <div>
+            <section className="h-screen flex items-center justify-center bg-red-100">
+                <h1 className="text-4xl font-bold">Section 1</h1>
+            </section>
+
+            <section className="h-screen flex items-center justify-center bg-blue-100">
+                <h1 className="text-4xl font-bold">Section 2</h1>
+            </section>
+        </div>
+    );
+}
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: true,
+  },
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
+})
